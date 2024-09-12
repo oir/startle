35f603cc5235b3e915bae11f6373e40d9dee06d8
Ozan İrsoy <oirs@pm.me>
2024-09-11 21:47:33 -0400

    Add help, flags, boolean parsing (#2)

--- startle/inspector.py
+++ startle/inspector.py
@@ -1,15 +1,77 @@
-from typing import get_type_hints, Optional, Union, get_origin, get_args
+from typing import get_type_hints, Callable, Optional, Union, get_origin, get_args
 import inspect
-from inspect import getmembers
 from .parser import Args
 import types
+import re
+from textwrap import dedent
 
 
-def make_args(func: callable) -> Args:
-    args = Args()
+def _parse_docstring(func: Callable) -> tuple[str, dict[str, str]]:
+    """
+    Parse the docstring of a function and return the brief and the arg descriptions.
+    """
+    brief = ""
+    arg_helps = {}
+    docstring = inspect.getdoc(func)
+    hints = get_type_hints(func)
 
+    if docstring:
+        lines = docstring.split("\n")
+
+        # first, find the brief
+        i = 0
+        while i < len(lines) and lines[i].strip() != "":
+            if brief:
+                brief += " "
+            brief += lines[i].strip()
+            i += 1
+
+        # first, find the Args section
+        args_section = ""
+        i = 0
+        while lines[i].strip() != "Args:":  # find the Args section
+            i += 1
+            if i >= len(lines):
+                break
+        i += 1
+        while i < len(lines) and lines[i].strip() != "":
+            args_section += lines[i] + "\n"
+            i += 1
+
+        if args_section:
+            args_section = dedent(args_section).strip()
+
+            # then, merge indented lines together
+            merged_lines = []
+            for line in args_section.split("\n"):
+                # if a line is indented, merge it with the previous line
+                if line.lstrip() != line:
+                    if not merged_lines:
+                        return brief, {}
+                    merged_lines[-1] += " " + line.strip()
+                else:
+                    merged_lines.append(line.strip())
+
+            # now each line should be an arg description
+            for line in merged_lines:
+                args_desc = re.search(r"(\S+)(?:\s+\(.*?\))?:(.*)", line)
+                param, desc = args_desc.groups()
+                param = param.strip()
+                desc = desc.strip()
+                if param in hints:
+                    arg_helps[param] = desc
+
+    return brief, arg_helps
+
+
+def make_args(func: Callable) -> Args:
     # Get the signature of the function
     sig = inspect.signature(func)
+
+    # Attempt to parse brief and arg descriptions from docstring
+    brief, arg_helps = _parse_docstring(func)
+
+    args = Args(brief=brief)
 
     # Helper function to normalize type annotations
     def normalize_type(annotation):
@@ -40,6 +102,8 @@ def make_args(func: callable) -> Args:
             required = True
             default = None
 
+        help = arg_helps.get(param_name, "")
+
         if param.kind == inspect.Parameter.POSITIONAL_ONLY:
             args.add(
                 normalized_annotation,
@@ -47,6 +111,7 @@ def make_args(func: callable) -> Args:
                 metavar=param_name,
                 required=required,
                 default=default,
+                help=help,
             )
         elif param.kind == inspect.Parameter.KEYWORD_ONLY:
             args.add(
@@ -54,6 +119,7 @@ def make_args(func: callable) -> Args:
                 names=[param_name],
                 required=required,
                 default=default,
+                help=help,
             )
         elif param.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD:
             args.add(
@@ -63,6 +129,7 @@ def make_args(func: callable) -> Args:
                 names=[param_name],
                 required=required,
                 default=default,
+                help=help,
             )
 
     return args
