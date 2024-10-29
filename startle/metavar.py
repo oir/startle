ddd02cdfea7adf8b88cc6f638c77907790020db7
oir <oirs@pm.me>
2024-10-28 22:17:08 -0400

    Add support for Literal[str]

--- startle/metavar.py
+++ startle/metavar.py
@@ -1,6 +1,7 @@
 from enum import Enum, IntEnum
 from inspect import isclass
 from pathlib import Path
+from typing import Literal, get_origin
 
 from ._type_utils import _strip_optional
 
@@ -18,8 +19,14 @@ def _get_metavar(type_: type) -> str:
     Get the metavar for a type hint.
     """
     type_ = _strip_optional(type_)
+    if get_origin(type_) is Literal:
+        if all(isinstance(value, str) for value in type_.__args__):
+            return "|".join(type_.__args__)
+
     if isclass(type_) and issubclass(type_, IntEnum):
         return "|".join([str(member.value) for member in type_])
+
     if isclass(type_) and issubclass(type_, Enum):
         return "|".join([member.value for member in type_])
+
     return _METAVARS.get(type_, "val")
---
+++ tests/test_parser/test_parse_literal.py
@@ -0,0 +1,50 @@
+import re
+from typing import Callable, Literal
+
+from _utils import check_args
+from pytest import mark, raises
+
+from startle.error import ParserValueError
+
+
+def check(draw: Callable, prefix: list[str]):
+    check_args(draw, prefix + ["square"], [], {"shape": "square"})
+    check_args(draw, prefix + ["circle"], [], {"shape": "circle"})
+    check_args(draw, prefix + ["triangle"], [], {"shape": "triangle"})
+
+    with raises(
+        ParserValueError,
+        match=re.escape(
+            "Cannot parse literal ('square', 'circle', 'triangle') from `rectangle`!"
+        ),
+    ):
+        check_args(draw, prefix + ["rectangle"], [], {})
+
+
+def check_with_default(draw: Callable, prefix: list[str]):
+    if not prefix:
+        check_args(draw, [], [], {"shape": "circle"})
+    check_args(draw, prefix + ["square"], [], {"shape": "square"})
+    check_args(draw, prefix + ["circle"], [], {"shape": "circle"})
+    check_args(draw, prefix + ["triangle"], [], {"shape": "triangle"})
+
+    with raises(
+        ParserValueError,
+        match=re.escape(
+            "Cannot parse literal ('square', 'circle', 'triangle') from `rectangle`!"
+        ),
+    ):
+        check_args(draw, prefix + ["rectangle"], [], {})
+
+
+@mark.parametrize("prefix", [[], ["--shape"], ["-s"]])
+def test_enum(prefix: list[str]):
+    def draw(shape: Literal["square", "circle", "triangle"]):
+        print(f"Drawing a {shape}.")
+
+    check(draw, prefix)
+
+    def draw_with_default(shape: Literal["square", "circle", "triangle"] = "circle"):
+        print(f"Drawing a {shape}.")
+
+    check_with_default(draw_with_default, prefix)
