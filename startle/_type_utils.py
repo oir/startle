baf3c3b95524235f989c3c882ad3e62707db3ca7
oir <oirs@pm.me>
2024-11-24 20:20:58 -0500

    Add register_type() for parsing user types, add test and example

--- startle/_type_utils.py
+++ startle/_type_utils.py
@@ -1,6 +1,6 @@
+import inspect
 import types
-import typing
-from typing import Optional, Union, get_args, get_origin
+from typing import Any, Optional, Union, get_args, get_origin
 
 
 def _strip_optional(type_: type):
@@ -8,14 +8,14 @@ def _strip_optional(type_: type):
     Strip the Optional type from a type hint. Given T1 | ... | Tn | None,
     return T1 | ... | Tn.
     """
-    if typing.get_origin(type_) is typing.Union:
-        args = typing.get_args(type_)
+    if get_origin(type_) is Union:
+        args = get_args(type_)
         if type(None) in args:
             args = tuple([arg for arg in args if arg is not type(None)])
             if len(args) == 1:
                 return args[0]
             else:
-                return typing.Union[args]
+                return Union[args]
 
     return type_
 
@@ -36,3 +36,29 @@ def _normalize_type(annotation):
         else:
             return Union[tuple(args)]
     return annotation
+
+
+def _shorten_type_annotation(annotation: Any) -> str:
+    origin = get_origin(annotation)
+    if origin is None:
+        # It's a simple type, return its name
+        if inspect.isclass(annotation):
+            return annotation.__name__
+        return str(annotation)
+
+    # Handle Optional types explicitly
+    if origin is Union or origin is types.UnionType:
+        args = get_args(annotation)
+        if type(None) in args:
+            args = [arg for arg in args if arg is not type(None)]
+            if len(args) == 1:
+                return f"{_shorten_type_annotation(args[0])} | None"
+            return " | ".join(_shorten_type_annotation(arg) for arg in args) + " | None"
+
+    # It's a generic type, process its arguments
+    args = get_args(annotation)
+    if args:
+        args_str = ", ".join(_shorten_type_annotation(arg) for arg in args)
+        return f"{origin.__name__}[{args_str}]"
+
+    return str(annotation)
