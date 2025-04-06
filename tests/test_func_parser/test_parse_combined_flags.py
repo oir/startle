c952caea48fd54d3c0745407fbb892fdd21f8288
oir <oirs@pm.me>
2025-04-06 18:45:20 -0400

    Improve combined flags test

--- tests/test_func_parser/test_parse_combined_flags.py
+++ tests/test_func_parser/test_parse_combined_flags.py
@@ -1,6 +1,6 @@
 import re
 
-from pytest import raises
+from pytest import mark, raises
 
 from startle.error import ParserOptionError
 
@@ -14,10 +14,10 @@ def jolt(
     A function that takes a name and three boolean flags.
 
     Args:
-        name (str): The name to use.
-        alpha (bool): The alpha flag.
-        beta (bool): The beta flag.
-        gamma (bool): The gamma flag.
+        name: The name to use.
+        alpha: The alpha flag.
+        beta: The beta flag.
+        gamma: The gamma flag.
     """
     pass
 
@@ -44,55 +44,92 @@ def test_only_flags():
         check_args(jolt, ["-abgb"], [], {})
 
 
-def test_flags_with_unary():
+@mark.parametrize("alpha", [True, False])
+@mark.parametrize("beta", [True, False])
+@mark.parametrize("gamma", [True, False])
+def test_flags_with_unary(alpha: bool, beta: bool, gamma: bool):
+    argstr = "-"
+    if alpha:
+        argstr += "a"
+    if beta:
+        argstr += "b"
+    if gamma:
+        argstr += "g"
+    argstr += "n"
     check_args(
-        jolt, ["-an", "zap"], ["zap"], {"alpha": True, "beta": False, "gamma": False}
+        jolt, [argstr, "zap"], ["zap"], {"alpha": alpha, "beta": beta, "gamma": gamma}
     )
-    check_args(
-        jolt, ["-abn", "zap"], ["zap"], {"alpha": True, "beta": True, "gamma": False}
-    )
-    check_args(
-        jolt, ["-abgn", "zap"], ["zap"], {"alpha": True, "beta": True, "gamma": True}
-    )
-    check_args(
-        jolt, ["-gn", "zap"], ["zap"], {"alpha": False, "beta": False, "gamma": True}
-    )
-    check_args(
-        jolt, ["-gbn", "zap"], ["zap"], {"alpha": False, "beta": True, "gamma": True}
-    )
-    check_args(
-        jolt, ["-gban", "zap"], ["zap"], {"alpha": True, "beta": True, "gamma": True}
-    )
-    check_args(
-        jolt,
-        ["-gb", "-an", "zap"],
-        ["zap"],
-        {"alpha": True, "beta": True, "gamma": True},
-    )
-
-    check_args(
-        jolt, ["-an=zap"], ["zap"], {"alpha": True, "beta": False, "gamma": False}
-    )
-    check_args(
-        jolt, ["-abn=zap"], ["zap"], {"alpha": True, "beta": True, "gamma": False}
-    )
-    check_args(
-        jolt, ["-abgn=zap"], ["zap"], {"alpha": True, "beta": True, "gamma": True}
-    )
-    check_args(
-        jolt, ["-gn=zap"], ["zap"], {"alpha": False, "beta": False, "gamma": True}
-    )
-    check_args(
-        jolt, ["-gbn=zap"], ["zap"], {"alpha": False, "beta": True, "gamma": True}
-    )
-    check_args(
-        jolt, ["-gban=zap"], ["zap"], {"alpha": True, "beta": True, "gamma": True}
-    )
-    check_args(
-        jolt, ["-gb", "-an=zap"], ["zap"], {"alpha": True, "beta": True, "gamma": True}
-    )
+    if len(argstr) > 3:
+        argstr1, argstr2 = argstr[:2], f"-{argstr[2:]}"
+        check_args(
+            jolt,
+            [argstr1, argstr2, "zap"],
+            ["zap"],
+            {"alpha": alpha, "beta": beta, "gamma": gamma},
+        )
 
     with raises(
         ParserOptionError, match=re.escape("Option `name` is missing argument!")
     ):
-        check_args(jolt, ["-an"], [], {})
+        check_args(jolt, [argstr], [], {})
+
+    argstr = f"{argstr}=zap"
+    check_args(jolt, [argstr], ["zap"], {"alpha": alpha, "beta": beta, "gamma": gamma})
+
+
+def jolt_multi(
+    names: list[str] = ["jolt"],
+    *,
+    alpha: bool = False,
+    beta: bool = False,
+    gamma: bool = False,
+):
+    """
+    A function that takes a name and three boolean flags.
+
+    Args:
+        names: Names to use.
+        alpha: The alpha flag.
+        beta: The beta flag.
+        gamma: The gamma flag.
+    """
+    pass
+
+
+@mark.parametrize("alpha", [True, False])
+@mark.parametrize("beta", [True, False])
+@mark.parametrize("gamma", [True, False])
+@mark.parametrize("names", [["zap"], ["zap", "zop"]])
+def test_flags_with_nary(alpha: bool, beta: bool, gamma: bool, names: list[str]):
+    argstr = "-"
+    if alpha:
+        argstr += "a"
+    if beta:
+        argstr += "b"
+    if gamma:
+        argstr += "g"
+    argstr += "n"
+    check_args(
+        jolt_multi,
+        [argstr, *names],
+        [names],
+        {"alpha": alpha, "beta": beta, "gamma": gamma},
+    )
+    if len(argstr) > 3:
+        argstr1, argstr2 = argstr[:2], f"-{argstr[2:]}"
+        check_args(
+            jolt_multi,
+            [argstr1, argstr2, *names],
+            [names],
+            {"alpha": alpha, "beta": beta, "gamma": gamma},
+        )
+
+    with raises(
+        ParserOptionError, match=re.escape("Option `names` is missing argument!")
+    ):
+        check_args(jolt_multi, [argstr], [], {})
+
+    argstr = f"{argstr}=zap"
+    check_args(
+        jolt_multi, [argstr], [["zap"]], {"alpha": alpha, "beta": beta, "gamma": gamma}
+    )
