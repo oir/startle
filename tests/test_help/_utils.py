77cf5d6534bcc03916ff9a955a44baea2e3b3e3b
Ozan İrsoy <oirs@pm.me>
2025-09-01 21:17:55 -0400

    Make some arguments kw-only (#105)

--- tests/test_help/_utils.py
+++ tests/test_help/_utils.py
@@ -18,7 +18,7 @@ def remove_trailing_spaces(text: str) -> str:
 def check_help_from_func(f: Callable, program_name: str, expected: str):
     console = Console(width=120, highlight=False, force_terminal=True)
     with console.capture() as capture:
-        make_args_from_func(f, program_name).print_help(console)
+        make_args_from_func(f, program_name=program_name).print_help(console)
     result = capture.get()
 
     console = Console(width=120, highlight=False, force_terminal=True)
@@ -32,7 +32,9 @@ def check_help_from_func(f: Callable, program_name: str, expected: str):
 def check_help_from_class(cls: type, brief: str, program_name: str, expected: str):
     console = Console(width=120, highlight=False, force_terminal=True)
     with console.capture() as capture:
-        make_args_from_class(cls, program_name, brief).print_help(console)
+        make_args_from_class(cls, program_name=program_name, brief=brief).print_help(
+            console
+        )
     result = capture.get()
 
     console = Console(width=120, highlight=False, force_terminal=True)
