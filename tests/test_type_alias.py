d81aa2c80755329ab2beab22ef303aa1667e2ae1
oir <oirs@pm.me>
2025-10-11 19:52:04 -0400

    add `parse` tests

--- tests/test_type_alias.py
+++ tests/test_type_alias.py
@@ -6,10 +6,12 @@ the new `type` syntax for type aliases, it is a syntax error in older versions,
 that cannot be gracefully handled with `if sys.version_info < (3, 12): ...`.
 """
 
-from typing import Annotated
+from dataclasses import dataclass
+from typing import Annotated, Callable
 
 from pytest import mark, raises
 
+from startle import parse
 from startle.error import ParserOptionError, ParserValueError
 
 from ._utils import check_args
@@ -121,3 +123,118 @@ def test_args_with_defaults(hi, count_t):
     #    check_args(hi, ["john", "-c", "3", "--"], [], {})
     with raises(ParserOptionError, match="Prefix `-` is not followed by an option!"):
         check_args(hi, ["john", "-"], [], {})
+
+
+@dataclass
+class ConfigDataClass:
+    """
+    A configuration class for the program.
+    """
+
+    count: int = 1
+    amount: MyFloat3 = 1.0
+    label: MyStr = "default"
+
+
+@mark.parametrize(
+    "count",
+    [
+        lambda c: ["--count", f"{c}"],
+        lambda c: [f"--count={c}"],
+        lambda c: ["-c", f"{c}"],
+        lambda c: [f"-c={c}"],
+    ],
+)
+@mark.parametrize(
+    "amount",
+    [
+        lambda a: ["--amount", f"{a}"],
+        lambda a: [f"--amount={a}"],
+        lambda a: ["-a", f"{a}"],
+        lambda a: [f"-a={a}"],
+    ],
+)
+@mark.parametrize(
+    "label",
+    [
+        lambda label: ["--label", f"{label}"],
+        lambda label: [f"--label={label}"],
+        lambda label: ["-l", f"{label}"],
+        lambda label: [f"-l={label}"],
+    ],
+)
+@mark.parametrize("Config", [ConfigDataClass])
+def test_class_with_all_defaults(
+    capsys,
+    count: Callable[[str], list[str]],
+    amount: Callable[[str], list[str]],
+    label: Callable[[str], list[str]],
+    Config: type,
+):
+    assert parse(Config, args=[]) == Config()
+
+    assert parse(Config, args=[*count("2")]) == Config(count=2)
+    assert parse(Config, args=["2"]) == Config(count=2)
+    assert parse(Config, args=[*amount("2.0")]) == Config(amount=2.0)
+    assert parse(Config, args=[*label("custom")]) == Config(label="custom")
+
+    # only count and amount
+    assert parse(Config, args=[*count("2"), *amount("2.0")]) == Config(
+        count=2, amount=2.0
+    )
+    assert parse(Config, args=["2", *amount("2.0")]) == Config(count=2, amount=2.0)
+    assert parse(Config, args=[*amount("2.0"), "2"]) == Config(count=2, amount=2.0)
+    assert parse(Config, args=["2", "2.0"]) == Config(count=2, amount=2.0)
+
+    # only count and label
+    expected = Config(count=2, label="custom")
+    assert parse(Config, args=[*count("2"), *label("custom")]) == expected
+    assert parse(Config, args=[*label("custom"), "2"]) == expected
+    assert parse(Config, args=["2", *label("custom")]) == expected
+    assert parse(Config, args=[*label("custom"), "2"]) == expected
+
+    # only amount and label
+    expected = Config(amount=2.0, label="custom")
+    assert parse(Config, args=[*amount("2.0"), *label("custom")]) == expected
+    assert parse(Config, args=[*label("custom"), *amount("2.0")]) == expected
+
+    # all three
+    expected = Config(count=2, amount=2.0, label="custom")
+    assert (
+        parse(Config, args=[*count("2"), *amount("2.0"), *label("custom")]) == expected
+    )
+    assert (
+        parse(Config, args=[*count("2"), *label("custom"), *amount("2.0")]) == expected
+    )
+    assert (
+        parse(Config, args=[*amount("2.0"), *label("custom"), *count("2")]) == expected
+    )
+    assert (
+        parse(Config, args=[*amount("2.0"), *count("2"), *label("custom")]) == expected
+    )
+    assert (
+        parse(Config, args=[*label("custom"), *count("2"), *amount("2.0")]) == expected
+    )
+    assert (
+        parse(Config, args=[*label("custom"), *amount("2.0"), *count("2")]) == expected
+    )
+    assert parse(Config, args=["2", *amount("2.0"), *label("custom")]) == expected
+    assert parse(Config, args=[*amount("2.0"), "2", *label("custom")]) == expected
+    assert parse(Config, args=[*amount("2.0"), *label("custom"), "2"]) == expected
+    assert parse(Config, args=["2", "2.0", *label("custom")]) == expected
+    assert parse(Config, args=["2", *label("custom"), "2.0"]) == expected
+    assert parse(Config, args=[*label("custom"), "2", "2.0"]) == expected
+    assert parse(Config, args=["2", "2.0", "custom"]) == expected
+
+    with raises(ParserOptionError, match="Unexpected option `unknown`!"):
+        parse(Config, args=["--unknown"], catch=False)
+    with raises(ParserValueError, match="Cannot parse integer from `a`!"):
+        parse(Config, args=["a"], catch=False)
+    with raises(ParserValueError, match="Cannot parse float from `a`!"):
+        parse(Config, args=["2", "a"], catch=False)
+    with raises(ParserOptionError, match="Option `count` is missing argument!"):
+        parse(Config, args=["--count"], catch=False)
+    with raises(ParserOptionError, match="Option `count` is missing argument!"):
+        parse(Config, args=["--amount", "1.0", "--count"], catch=False)
+    with raises(ParserOptionError, match="Option `count` is multiply given!"):
+        parse(Config, args=["--count", "2", "--count", "3"], catch=False)
